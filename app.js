ac76f1a6002ac5cbb75bcf669891305e05a3fbb6
HYEONGYUN\guddb <guddbs5545@naver.com>
2024-12-27 17:26:13 +0900

    middleware 실습

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 import express from "express";
 const app = express();
 
-app.get("/", (req, res, next)=>{
-    console.log(req)
-    res.send("Hello")
+app.all('/api', ()=>{
+    console.log('all');
+    next();
 })
 
+app.use('/sky', ()=>{
+    console.log('use');
+    next();
+})
 
+app.get("/", (req, res, next)=>{
+    console.log("first")
+    next("route");
+},
+(req,res, next) => {
+    console.log("first2")
+    res.send("Hello")
+    next()
+})
 
-
+app.use((err, req,res,next)=>{
+    res.status(500).send("Sorry, try later!");
+    next();
+})
 app.listen(8080);
